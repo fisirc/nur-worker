054f64503ef76ae2af7912784549be34f80110dd
Paolo Flores <pflores.fisi22@gmail.com>
2025-06-12 17:25:00 -0500

    broken: add s3 sdk for rust

--- worker/src/main.rs
+++ worker/src/main.rs
@@ -1,3 +1,7 @@
-fn main() {
-    println!("Hello, world!");
+use aws_sdk_s3 as s3;
+
+#[::tokio::main]
+async fn main() {
+    let config = aws_config::load_from_env().await;
+    let client = aws_sdk_s3::Client::new(&config);
 }
