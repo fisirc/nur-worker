440d50dcc49a944ed0b8319fa755fcbf434832de
Paolo Flores <pflores.fisi22@gmail.com>
2025-06-15 15:58:55 -0500

    add logger

---
+++ worker/src/logger.rs
@@ -0,0 +1,63 @@
+use core::fmt;
+use std::sync::atomic::{AtomicUsize, Ordering};
+
+use env_logger::fmt::{Color, Style, StyledValue};
+use log::Level;
+
+static MAX_MODULE_WIDTH: AtomicUsize = AtomicUsize::new(0);
+
+pub fn build_logger() -> env_logger::Builder {
+    let mut builder = env_logger::Builder::new();
+
+    builder.format(|f, record| {
+        use std::io::Write;
+        let target = record.target();
+        let max_width = max_target_width(target);
+
+        let mut style = f.style();
+        let level = colored_level(&mut style, record.level());
+
+        let mut style = f.style();
+        let target = style.set_bold(true).value(Padded {
+            value: target,
+            width: max_width,
+        });
+
+        let time = f.timestamp_millis();
+        writeln!(f, "{} {} {} > {}", time, level, target, record.args(),)
+    });
+
+    builder.parse_env("RUST_LOG").format_indent(None);
+    builder
+}
+
+struct Padded<T> {
+    value: T,
+    width: usize,
+}
+
+impl<T: fmt::Display> fmt::Display for Padded<T> {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        write!(f, "{: <width$}", self.value, width = self.width)
+    }
+}
+
+fn max_target_width(target: &str) -> usize {
+    let max_width = MAX_MODULE_WIDTH.load(Ordering::Relaxed);
+    if max_width < target.len() {
+        MAX_MODULE_WIDTH.store(target.len(), Ordering::Relaxed);
+        target.len()
+    } else {
+        max_width
+    }
+}
+
+fn colored_level<'a>(style: &'a mut Style, level: Level) -> StyledValue<'a, &'static str> {
+    match level {
+        Level::Trace => style.set_color(Color::Magenta).value("TRACE"),
+        Level::Debug => style.set_color(Color::Blue).value("DEBUG"),
+        Level::Info => style.set_color(Color::Green).value("INFO "),
+        Level::Warn => style.set_color(Color::Yellow).value("WARN "),
+        Level::Error => style.set_color(Color::Red).value("ERROR"),
+    }
+}
