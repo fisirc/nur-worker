e7d99078a674471f6ff73d63cceae625f183833d
Paolo Flores <pflores.fisi22@gmail.com>
2025-06-18 22:28:08 -0500

    fix wasm server

--- worker/src/server.rs
+++ worker/src/server.rs
@@ -1,11 +1,15 @@
 use std::{io, net::SocketAddr};
-use tokio::{io::{AsyncReadExt, AsyncWriteExt}, net::ToSocketAddrs};
+use tokio::io::{AsyncReadExt, AsyncWriteExt};
+use tokio::net::ToSocketAddrs;
+use tokio::select;
 
-use wasmer::{imports, AsStoreMut, AsStoreRef, FunctionEnv, FunctionEnvMut, Instance, Module, Store};
+use crate::intrinsics;
+use wasmer::{FunctionEnv, Instance, Module, Store, imports};
 
 static WASM: &'static [u8] = include_bytes!("../test.wasm");
 
-const EXPORTED_HANDLER_SYMBOL_NAME: &str = "handle_request";
+const EXPORTED_POLL_HANDLER_SYMBOL_NAME: &str = "poll_stream";
+const EXPORTED_ALLOC_SYMBOL_NAME: &str = "alloc";
 
 pub struct Server {
     listener: tokio::net::TcpListener,
@@ -26,12 +30,13 @@ impl Server {
         }
     }
 
-    async fn handle_conn(mut socket: tokio::net::TcpStream, addr: SocketAddr) {
-        socket.write(":3\n".as_bytes()).await.unwrap();
+    async fn handle_conn(socket: tokio::net::TcpStream, addr: SocketAddr) {
         // TODO handshake
         // TODO fetch from S3
         // TODO unzpip
+        let (mut socket_read_half, mut socket_write_half) = socket.into_split();
 
+        log::warn!("Instatiating wasm module...");
         let mut store = Store::default();
         let module = match Module::new(&store, WASM) {
             Ok(module) => module,
@@ -39,33 +44,24 @@ impl Server {
                 log::error!("Failed to compile WebAssembly module: {e}");
                 // TODO: send error back
                 return;
-            },
+            }
         };
 
-        struct FuncEnv {
-            memory: Option<wasmer::Memory>,
-        }
+        let (tx, rx) = flume::unbounded::<intrinsics::NurWasmMessage>();
 
-        let env = FunctionEnv::new(&mut store, FuncEnv {
-            memory: None,
-        });
-
-        fn print_str(env: FunctionEnvMut<FuncEnv>, ptr: u32, len: i32) {
-            let data = env.data();
-            let store = env.as_store_ref();
-
-            let memory = data.memory.as_ref().unwrap();
-            let bytes = memory.view(&store);
-            let slice = &bytes.copy_range_to_vec(ptr as u64..(ptr + len as u32) as u64).unwrap();
-            let str_data = String::from_utf8_lossy(slice);
-            log::info!("wa: {}", str_data);
-        }
-
-        let print_str_typed = wasmer::Function::new_typed_with_env(&mut store, &env, print_str);
+        let func_env = FunctionEnv::new(
+            &mut store,
+            intrinsics::NurFunctionEnv {
+                memory: None,
+                channel_tx: tx,
+            },
+        );
 
         let import_object = imports! {
-            "env" => {
-                "print_str" => print_str_typed,
+            "nur" => {
+                "nur_log" => wasmer::Function::new_typed_with_env(&mut store, &func_env, intrinsics::nur_log),
+                "nur_send" => wasmer::Function::new_typed_with_env(&mut store, &func_env, intrinsics::nur_send),
+                "nur_end" => wasmer::Function::new_typed_with_env(&mut store, &func_env, intrinsics::nur_end),
             },
         };
 
@@ -75,33 +71,119 @@ impl Server {
                 // TODO: send error back
                 log::error!("Failed to instantiate WebAssembly module: {e}");
                 return;
-            },
+            }
         };
 
-        env.as_mut(&mut store).memory = Some(instance.exports.get_memory("memory").unwrap().clone());
+        let instance_memory = instance.exports.get_memory("memory").unwrap().clone();
 
-        let handle_request = match instance.exports.get_function(EXPORTED_HANDLER_SYMBOL_NAME) {
-            Ok(func) => func,
+        func_env.as_mut(&mut store).memory = Some(instance_memory.clone());
+
+        let wasm_poll_stream = match instance
+            .exports
+            .get_function(EXPORTED_POLL_HANDLER_SYMBOL_NAME)
+        {
+            Ok(func) => func.clone(),
             Err(e) => {
-                log::error!("Failed to get exported function '{}': {}", EXPORTED_HANDLER_SYMBOL_NAME, e);
+                log::error!(
+                    "Failed to get exported function '{}': {}",
+                    EXPORTED_POLL_HANDLER_SYMBOL_NAME,
+                    e
+                );
                 return;
-            },
+            }
         };
+
+        let wasm_alloc = match instance.exports.get_function(EXPORTED_ALLOC_SYMBOL_NAME) {
+            Ok(func) => func.clone(),
+            Err(e) => {
+                log::error!(
+                    "Failed to get exported function '{}': {}",
+                    EXPORTED_ALLOC_SYMBOL_NAME,
+                    e
+                );
+                return;
+            }
+        };
+        log::warn!("Wasm module instantiated!");
 
         let mut buf = vec![0; 1024];
-        loop {
-            match socket.read(&mut buf).await {
-                Ok(0) => {
-                    log::info!("Connection closed by {addr}");
-                    break;
-                },
-                Ok(n) => {
-                    handle_request.call(&mut store, &[]).unwrap();
+
+        let listen_wasm_messages_task = tokio::spawn(async move {
+            loop {
+                let msg = rx.recv_async().await;
+                match msg {
+                    Ok(intrinsics::NurWasmMessage::Abort) => {
+                        log::info!("Aborting connection with {addr}");
+                        socket_write_half.shutdown().await.unwrap();
+                        break;
+                    }
+                    Ok(intrinsics::NurWasmMessage::SendData { data }) => {
+                        if let Err(e) = socket_write_half.write(&data).await {
+                            log::error!("Failed to send data to {addr}: {e}");
+                            break;
+                        }
+                    }
+                    Err(flume::RecvError::Disconnected) => {
+                        log::info!("Channel closed, aborting connection with {addr}");
+                        break;
+                    }
                 }
-                Err(e) => {
-                    log::error!("Error reading from socket: {}", e);
-                    break;
-                }
+            }
+        });
+
+        let read_connection = tokio::spawn(async move {
+            loop {
+                let read_n = { socket_read_half.read(&mut buf).await };
+
+                match read_n {
+                    Ok(0) => {
+                        log::info!("Connection closed by {addr}");
+                        break;
+                    }
+                    Ok(n) => {
+                        // TODO: unwraps should abort the execution safely
+                        let ptr = wasm_alloc
+                            .call(&mut store, &[wasmer::Value::I32(n as i32)])
+                            .unwrap();
+
+                        let ptr = &ptr[0];
+
+                        let ptr_num = *match ptr {
+                            wasmer::Value::I32(num) => num,
+                            _ => {
+                                log::error!(
+                                    "Expected I32 return value from alloc function. Aborting."
+                                );
+                                return;
+                            }
+                        };
+
+                        instance_memory
+                            .view(&store)
+                            .write(ptr_num as u64, &buf[..n])
+                            .unwrap();
+
+                        wasm_poll_stream
+                            .call(
+                                &mut store,
+                                &[wasmer::Value::I32(ptr_num), wasmer::Value::I32(n as i32)],
+                            )
+                            .unwrap();
+                    }
+                    Err(e) => {
+                        log::error!("Error reading from socket: {}", e);
+                        break;
+                    }
+                };
+            }
+        });
+
+        select! {
+            _ = listen_wasm_messages_task => {
+                log::info!("WASM message listener task completed for {addr}");
+            },
+            _ = read_connection => {
+                log::info!("Read connection task completed for {addr}");
             }
         }
     }
