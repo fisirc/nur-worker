8e3cca89abd5fbf7f5de06c54cf541d7a8be3a25
luedu1103 <luedu1103@gmail.com>
2025-06-21 23:58:07 -0500

    nur test

--- examples/hello_world/src/lib.rs
+++ examples/hello_world/src/lib.rs
@@ -35,7 +35,7 @@ static RESPONSE: &str = "HTTP/1.1 200 OK\r\n\
 Content-Type: application/json\r\n\
 Content-Length: 27\r\n\
 \r\n\
-{\"msg\": \"Hello world, wa!\"}";
+{\"msg\": \"Hello world, wasm!\"}";
 
 // This will be called by wasmer
 #[unsafe(no_mangle)]
