5d95584aec6f2b09291d134bb112b27fc9a95f55
Paolo Flores <pflores.fisi22@gmail.com>
2025-06-25 01:01:28 -0500

    implement log passing to log service

--- worker/src/intrinsics.rs
+++ worker/src/intrinsics.rs
@@ -1,16 +1,14 @@
 use std::ops::Range;
-use uuid::Uuid;
 use wasmer::{AsStoreRef, FunctionEnvMut};
 
 pub struct NurFunctionEnv {
-    pub function_id: Uuid,
     pub memory: Option<wasmer::Memory>,
     pub channel_tx: flume::Sender<NurWasmMessage>,
 }
 
 pub enum NurWasmMessage {
     Abort,
-    LogMessage { msg: String },
+    LogMessage { log: String },
     SendData { data: Vec<u8> },
 }
 
@@ -30,9 +28,13 @@ pub fn nur_log(env: FunctionEnvMut<NurFunctionEnv>, ptr: i32, len: i32) {
 
     let msg = String::from_utf8_lossy(memory_slice.as_slice());
 
-    data.channel_tx.send(NurWasmMessage::LogMessage {
-        msg: msg.to_string(),
-    });
+    data.channel_tx
+        .send(NurWasmMessage::LogMessage {
+            log: msg.to_string(),
+        })
+        .unwrap_or_else(|e| {
+            log::error!("nur_log: Failed to send log message \"{msg}\" through channel: {e}");
+        });
 }
 
 pub fn nur_send(env: FunctionEnvMut<NurFunctionEnv>, ptr: i32, len: i32) {
@@ -53,12 +55,19 @@ pub fn nur_send(env: FunctionEnvMut<NurFunctionEnv>, ptr: i32, len: i32) {
         .send(NurWasmMessage::SendData {
             data: memory_slice.to_owned(),
         })
-        .unwrap();
+        .unwrap_or_else(|e| {
+            log::error!("nur_send: Failed to send data \"{memory_slice:?}\" through channel: {e}");
+        });
 }
 
 /// Aborts with the given message described by a fat ointer in memory.
 pub fn nur_end(mut env: FunctionEnvMut<NurFunctionEnv>) {
     log::trace!("nur_end()");
     let data = env.data_mut();
-    data.channel_tx.send(NurWasmMessage::Abort).unwrap();
+
+    data.channel_tx
+        .send(NurWasmMessage::Abort)
+        .unwrap_or_else(|e| {
+            log::error!("nur_end: Failed to send end message through channel: {e}");
+        });
 }
